d4ca222c72c198a46871db8a04addf2b86824d40
Alex Melo <alexmelo84@hotmail.com>
2024-10-28 16:20:28 -0300

    Adicionando validação e formatação dos dados de usuário

--- api/src/modelos/usuarios.go
+++ api/src/modelos/usuarios.go
@@ -1,6 +1,8 @@
 package modelos
 
 import (
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -12,4 +14,40 @@ type Usuario struct {
 	Email string `json:"email,omitempty"`
 	Senha string `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criado_em,omitempty"`
+}
+
+// Preparar valida e formata os dados do usuário
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("O senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
